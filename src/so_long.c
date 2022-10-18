737e15c9736124d671210941c3c8b1d581dbc604
gsmereka <gabrielsmereka200@gmail.com>
2022-10-18 10:21:41 -0300

    realease version v2

--- src/so_long.c
+++ src/so_long.c
@@ -6,14 +6,11 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 11:30:38 by gsmereka          #+#    #+#             */
-/*   Updated: 2022/10/15 23:28:53 by gsmereka         ###   ########.fr       */
+/*   Updated: 2022/10/18 10:20:58 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/so_long.h"
-
-// make && make clean && clang so_long.a 
-// -lX11 -lXext -lmlx && clear && ./a.out ./src/maps
 
 int	main(int argc, char *argv[])
 {
